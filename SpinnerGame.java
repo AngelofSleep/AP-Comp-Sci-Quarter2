5568ab8ead68a7c2baf020504a4ddd856ae9356c
Kathleen Reece <123769977+AngelofSleep@users.noreply.github.com>
2023-01-27 15:30:45 -0500

    Update SpinnerGame.java

--- SpinnerGame.java
+++ SpinnerGame.java
@@ -1,18 +1,18 @@
 import java.util.*;
-class SpinnerGame 
+class SpinnerGame //requires Spinner.java to run properly
 {
   public static void main(String[] args) 
   {
-    Scanner input = new Scanner(System.in);
+    Scanner mae = new Scanner(System.in);
 
     int board, posBlue = 0, posPink = 0, posGreen = 0;
     
-    Spinner blue = new Spinner("blue",5);
+    Spinner blue = new Spinner("blue",5); //each "player" has their own spinner
     Spinner pink = new Spinner("pink", 3);
     Spinner green = new Spinner("green", 4);
 
     System.out.println("How many spaces is the board?");
-    board = input.nextInt();
+    board = mae.nextInt();
     while(posBlue < board && posPink< board && posGreen < board)
     {
       posBlue += blue.spin();
