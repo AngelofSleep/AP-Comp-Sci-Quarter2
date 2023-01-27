a2c9c1de5b8f1af9b514db976ed2de09cf123757
Kathleen Reece <123769977+AngelofSleep@users.noreply.github.com>
2023-01-27 15:43:38 -0500

    Update APLine.java

--- APLine.java
+++ APLine.java
@@ -1,7 +1,8 @@
-public class APLine
+public class APLine //created as a response to a practice AP exam question
+                    //makes line ax + by + c = 0, where neither a or b is 0 and a,b,c all integers
 {
    private int a, c;
-   private double b, slope;
+   private double b; //b + e doubles so getSlope may return a double if value of slope isn't an integer
    public APLine(int d, double e, int f)
    {
       a = d;
