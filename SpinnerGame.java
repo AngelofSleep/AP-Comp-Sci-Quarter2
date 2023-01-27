3cd0757cf028704f7d8c0e47902efc7ec99e6275
Kathleen Reece <123769977+AngelofSleep@users.noreply.github.com>
2023-01-27 15:27:40 -0500

    Add files via upload

---
+++ SpinnerGame.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class SpinnerGame 
+{
+  public static void main(String[] args) 
+  {
+    Scanner input = new Scanner(System.in);
+
+    int board, posBlue = 0, posPink = 0, posGreen = 0;
+    
+    Spinner blue = new Spinner("blue",5);
+    Spinner pink = new Spinner("pink", 3);
+    Spinner green = new Spinner("green", 4);
+
+    System.out.println("How many spaces is the board?");
+    board = input.nextInt();
+    while(posBlue < board && posPink< board && posGreen < board)
+    {
+      posBlue += blue.spin();
+      posPink += pink.spin();
+      posGreen += green.spin();
+
+      System.out.println("Blue is currently at " + posBlue);
+      System.out.println("Pink is currently at " + posPink);
+      System.out.println("Green is currently at " + posGreen);
+
+      if(posBlue >= board)
+        System.out.println("Blue is the winner!");
+      else if(posPink >= board)
+        System.out.println("Pink is the winner!");
+      else if(posGreen >= board)
+         System.out.println("Green is the winner!");
+
+    }
+  }
+}
